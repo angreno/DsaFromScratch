a45783661fa62a0ef6991b7face8ed94f9e93f61
Angreno <singhkunaliitianbusiness1@gmail.com>
2024-11-09 23:38:26 +0530

    day 3

--- PatternPrinting/p1_solution.cpp
+++ PatternPrinting/p1_solution.cpp
@@ -5,11 +5,11 @@ int main(){
     int row,column;
     cin>>row;
     cin>>column;
-    for(int i =0 ; i< row ; i++){
-        cout<<"*"<<endl;
+    for(int i =0 ; i<row ; i++){
         for(int i=0 ; i<column ; i++){
             cout<<"*";
         }
+    cout<<endl;
 
     }
 }
