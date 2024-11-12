0ea7bc9f96518748b2d83099dba47728637581cf
Angreno <singhkunaliitianbusiness1@gmail.com>
2024-11-13 00:16:08 +0530

    files add

--- PatternPrinting/p6_solution.cpp
+++ PatternPrinting/p6_solution.cpp
@@ -4,12 +4,11 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
-    for(int i=0;i<=n;i++){
-        for(int j=n-i ; j>0;j--){
-            
-            cout<<'*';
+    for(int i=0;i<n;i++){
+        for(int j=1; j<=n-i;j++){
+            cout<<j;
+
         }
         cout<<endl;
     }
-    
 }
