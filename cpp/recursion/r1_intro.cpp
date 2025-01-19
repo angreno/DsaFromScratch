9eb403102e6afff07ecf8fa1842d1cabf0a78a99
Angreno <singhkunaliitianbusiness1@gmail.com>
2025-01-20 00:58:11 +0530

    recursion

--- cpp/recursion/r1_intro.cpp
+++ cpp/recursion/r1_intro.cpp
@@ -1,13 +1,18 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int countl = 0;
+
+
 void fun(char name){
-  int count =0;
-  if(count==7){
+  //static int count =0;
+  if(countl==7){
     return ;
   }
   else{
     cout<<"my name first letter is "<<name<<endl;
-    count++;
+    countl++;
+    
     fun(name);
   }
 
@@ -19,7 +24,6 @@ void fun(char name){
 }
 
 int main(){
-  int count =0;
   //recursion function which call itself until specified condition is met is called recursive function and this process of calling function which calls itself is knowns as recursion
   // when memory of stack get filled by function calls and cannot call more function it is known as stack overflow
 
