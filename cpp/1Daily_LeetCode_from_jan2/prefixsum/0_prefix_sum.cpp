d62128536aa0672bb891e9abff9383c56a558848
Angreno <singhkunaliitianbusiness1@gmail.com>
2025-01-22 21:37:17 +0530

    prefix sum

---
+++ cpp/1Daily_LeetCode_from_jan2/prefixsum/0_prefix_sum.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+int main(){
+  int arr []={1,2,4,5,6,7,7,7,34};
+  int query[2];
+  cout<<"type query index 1"<<endl;
+  cin>>query[0];
+  cout<<"type query index 2"<<endl;
+  cin>>query[1];
+  cout<<"your query array is "<<endl;
+  for(int i =0 ; i<=1; i++){
+    cout<<query[i]<<" ";
+  }
+  int j = query[0];
+  int k = query [1];
+  int n = 9;
+  int s=0;
+
+  //prefix sum algorithm by me 
+  cout<<"\n updated prefix array"<<endl;
+  for(int i =0 ; i<=n-1;i++ ){
+    s=arr[i]+s;
+      
+    arr[i]=s;
+    cout<<arr[i]<<" ";
+    
+    
+
+
+  }
+  //query handler
+  if (j == 0) {
+        cout<< arr[k]; // Sum from 0 to k
+    } else {
+        cout<< arr[k] - arr[j - 1]; // Sum from j to k
+    }
+  
+
+}
