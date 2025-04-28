3cb2fa93ed2fcf85c8d31dff3eedd0cb4ebfd613
angreno <singhkunaliitianbusiness1@gmail.com>
2025-04-28 23:58:10 +0530

    a fresh start

---
+++ OOps_concepts/o1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+class agent{
+  char name[100];
+  int health;
+};
+
+
+
+int main(){
+agent reyna;
+cout<<sizeof(reyna);
+reyna.name;
+reyna.health;
+
+
+  
+}
