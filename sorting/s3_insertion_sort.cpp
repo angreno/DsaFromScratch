99f82aea364c14e2494a1d810168883d1fcf2a00
angreno <singhkunaliitianbusiness1@gmail.comgit config --global user.name>
2025-06-26 19:39:29 +0530

    insertion sort updated

--- sorting/s3_insertion_sort.cpp
+++ sorting/s3_insertion_sort.cpp
@@ -5,16 +5,13 @@ int main() {
   int arr [5] = {5,3,4,1,2};
   int n = 5;
   //insertion sort implementation 
-  for(int i =1 ;i<n;i++){
-    
-    int element = arr[i];
-     
-    for(int j = i-1 ; j>=0 ; j--){
-      if(element < arr[j]){
-        arr[j+1] = arr[j];
-
-      }
-
+  for(int i =0 ;i<n;i++){
+    int j = i ;
+    while(j>0 && arr[j-1] > arr[j]){
+      int temp = arr[j-1];
+      arr[j-1] = arr[j];
+      arr[j] = temp;
+      j--;
     }
   }
 
