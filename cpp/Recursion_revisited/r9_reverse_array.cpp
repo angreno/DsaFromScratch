82cef400d04c16a9c8c6e35cd7524f958cd6bfd7
angreno <singhkunaliitianbusiness1@gmail.com>
2025-06-29 01:58:50 +0530

    single parameter reverse

--- cpp/Recursion_revisited/r9_reverse_array.cpp
+++ cpp/Recursion_revisited/r9_reverse_array.cpp
@@ -1,25 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 //using two pointers
-void rev(vector<int>arr ,int i,int n){
-  if(i>n){
+void rev(vector<int>arr ,int i){
+  if(i>=arr.size()){
     for(int i =0;i<=3;i++){
       cout<<arr[i]<<endl;
   }
 
     return;
   }
-  int temp = arr[i];
-  arr[i]=arr[n];
-  arr[n]=temp;
-  rev(arr,i+1,n-1);
+  arr[i]=arr[arr.size()-1-i];
+
+
+  rev(arr,i+1);
 }
 
 
 int main(){
   vector<int> arr ={1,2,3,4};
   cout<<"Hii"<<endl;
-  rev(arr,0,3);
+  rev(arr,0);
   for(int i =0;i<=3;i++){
     cout<<arr[i];
   }
