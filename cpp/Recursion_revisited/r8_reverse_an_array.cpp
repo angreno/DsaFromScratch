6328930663bea44e8b4241cb7e30eb2aae45e253
angreno <singhkunaliitianbusiness1@gmail.com>
2025-06-29 01:50:39 +0530

    reverse array using recursion

--- cpp/Recursion_revisited/r8_reverse_an_array.cpp
+++ cpp/Recursion_revisited/r8_reverse_an_array.cpp
@@ -1,15 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
+void rev(vector<int>arr ,int i,int n){
+  if(i>n){
+    for(int i =0;i<=3;i++){
+      cout<<arr[i]<<endl;
+  }
 
+    return;
+  }
+  int temp = arr[i];
+  arr[i]=arr[n];
+  arr[n]=temp;
+  rev(arr,i+1,n-1);
+}
 
 
 int main(){
   vector<int> arr ={1,2,3,4};
-  for(int i =0 ; i<3;i++){
-    int temp = arr[i];
-    arr[i]=arr[i+1];
-    arr[i+1]=temp;
-  }
+  cout<<"Hii"<<endl;
+  rev(arr,0,3);
   for(int i =0;i<=3;i++){
     cout<<arr[i];
   }
