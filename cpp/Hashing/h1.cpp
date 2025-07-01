f164684922e24448894c4ae8fba42aedb1b76e64
angreno <singhkunaliitianbusiness1@gmail.com>
2025-07-01 22:48:47 +0530

    merge sort

---
+++ cpp/Hashing/h1.cpp
@@ -0,0 +1,17 @@
+//in naive term we can say that hashing is something which prestores and fetches 
+#include <bits/stdc++.h>
+using namespace std;
+
+void hashing(int arr[], int n){
+  int newarr[n];
+  
+
+
+
+}
+
+int main() {
+  int arr[5]={1,2,3,4,4};
+  hashing(arr,5);
+    return 0;
+}
