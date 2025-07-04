356505b7add48692f8634766fc18267bc7db9fbe
angreno <singhkunaliitianbusiness1@gmail.com>
2025-07-04 23:27:53 +0530

    dry run

--- sorting/s4_merge_sort.cpp
+++ sorting/s4_merge_sort.cpp
@@ -1,7 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int mergesort(int arr,int low , int high){
+    //divide base condition
+    if(high >= low){
+        return;
+
+    }
+     int mid = (low + high )/2;
+     mergesort(arr,low, mid);
+     mergesort(arr,mid+1,high);
+}
+
 int main() {
+    int arr [5]= {3,2,4,1,3};
+    
+
 
     return 0;
 }
